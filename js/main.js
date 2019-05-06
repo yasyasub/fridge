6e172d7ee8e488f940ab5ccb8ff8cc9bcbca8f9c
Chainii_packetic <yasyasub676@gmail.com>
2019-05-06 19:56:04 +0300

    refactoring main.js and plugins.js

--- js/main.js
+++ js/main.js
@@ -1,61 +1,45 @@
 (function($) {
-
 	"use strict";
-   $(window).load(function() {
+		$(window).load(function() {
     	$("#loader").fadeOut("slow", function(){
         $("#preloader").delay(300).fadeOut("slow");
       });
   	})
-   $(window).on('scroll', function() {
-
-		var y = $(window).scrollTop(),
-		    topBar = $('header');
-
-	   if (y > 1) {
-	      topBar.addClass('sticky');
-	   }
+		$(window).on('scroll', function() {
+			var y = $(window).scrollTop(),
+			topBar = $('header');
+			if (y > 1) {
+				topBar.addClass('sticky');
+	   	}
       else {
          topBar.removeClass('sticky');
       }
-
-	});
-	var toggleButton = $('.menu-toggle'),
-       nav = $('.main-navigation');
-
-   toggleButton.on('click', function(event){
-		event.preventDefault();
-
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
-	});
-
+		});
+		var toggleButton = $('.menu-toggle'),
+		nav = $('.main-navigation');
+		toggleButton.on('click', function(event){
+			event.preventDefault();
+			toggleButton.toggleClass('is-clicked');
+			nav.slideToggle();
+		});
   	if (toggleButton.is(':visible')) nav.addClass('mobile');
-
   	$(window).resize(function() {
-   	if (toggleButton.is(':visible')) nav.addClass('mobile');
-    	else nav.removeClass('mobile');
+   		if (toggleButton.is(':visible')) nav.addClass('mobile');
+    		else nav.removeClass('mobile');
   	});
-
   	$('#main-nav-wrap li a').on("click", function() {
-
-   	if (nav.hasClass('mobile')) {
-   		toggleButton.toggleClass('is-clicked');
-   		nav.fadeOut();
-   	}
-  	});
-
-	var sections = $("section"),
-	navigation_links = $("#main-nav-wrap li a");
-
-	sections.waypoint( {
-
+   		if (nav.hasClass('mobile')) {
+   			toggleButton.toggleClass('is-clicked');
+   			nav.fadeOut();
+			}
+		});
+		var sections = $("section"),
+		navigation_links = $("#main-nav-wrap li a");
+		sections.waypoint( {
        handler: function(direction) {
-
 		   var active_section;
-
-			active_section = $('section#' + this.element.id);
-
-			if (direction === "up") active_section = active_section.prev();
+			 active_section = $('section#' + this.element.id);
+			 if (direction === "up") active_section = active_section.prev();
 
 			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
